6b3c8f010200b966bcabee7c0d0cfe6a31cb10cd
neerajraja <75340840+neerajraja@users.noreply.github.com>
2023-04-06 02:21:31 -0400

    Delete PromotionRepository.java

--- PromotionRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.repository;
-
-import org.example.model.Promotion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PromotionRepository extends JpaRepository<Promotion, Integer> {
-}

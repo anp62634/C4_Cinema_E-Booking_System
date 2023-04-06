9dc3c9a37a43af7370315cdfd3b4f36c2a6b5f35
cav90839 <75345004+cav90839@users.noreply.github.com>
2023-04-06 03:36:32 -0400

    Delete MovieRepository.java

--- MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.repository;
-
-import org.example.model.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Integer> {
-}

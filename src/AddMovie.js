cd95d1599c5cedb5014c6464ec46b015fd170d16
anp62634 <anp62634@uga.edu>
2023-04-07 16:15:25 -0400

    fixed movie so its pushing to database correctly

--- src/AddMovie.js
+++ src/AddMovie.js
@@ -12,15 +12,22 @@ export default function AddMovie() {
     const[director, setDirector]=useState('');
     const[producer, setProducer]=useState('');
     const[desc, setSynopsis]=useState('');
+    const[checked, setChecked]=useState(false);
+    const[isShowing, setIsShowing]=useState(0);
     const[runtime, setRuntime]=useState('0');
 
     const cancelAddHandler = () => {
         navigate('/adminview/managemovies');
     };
+
+    const handleShowing = () => {
+        setChecked(!checked);
+        setIsShowing(isShowing ? 0 : 1);
+    }
      
     const confirmAddHandler = (e) => {
         e.preventDefault()
-        const movie={title, genre, cast, director, producer, desc, runtime}
+        const movie={title, genre, cast, director, producer, desc, isShowing, runtime}
         console.log(movie)
         fetch("http://localhost:8080/movie/add", {
             method:"POST",
@@ -42,7 +49,7 @@ export default function AddMovie() {
                     <label htmlFor="movie-name">Movie Name</label>
                     <input type="text" name="movie-name" value={title} onChange={(e)=>setTitle(e.target.value)} required/>
                     <label htmlFor="movie-rating">Movie Rating</label>
-                    <input type="text" name="movie-rating" placeholder="" /**value={mpaa} onChange={(e)=>setMpaa(e.target.value)} */ required/>
+                    <input type="text" name="movie-rating" placeholder="" /**value={mpaa} onChange={(e)=>setMpaa(e.target.value)} *//>
                     <label htmlFor="movie-category">Movie Category</label>
                     <input type="text" name="movie-category" placeholder="" value={genre} onChange={(e)=>setGenre(e.target.value)} required/>
                     <label htmlFor="movie-cast">Movie Cast</label>
@@ -55,7 +62,7 @@ export default function AddMovie() {
                     <input type="text" name="movie-description" placeholder="" value={desc} onChange={(e)=>setSynopsis(e.target.value)} required/>
                     <div>
                         <label htmlFor="currentlyshowing">Will Movie be Currently Showing</label>
-                        <input type="checkbox" name="currentlyshowing" />
+                        <input type="checkbox" name="currentlyshowing"  onChange={handleShowing}/>
                     </div>
                     <label htmlFor="movie-showdate">Movie Show Date</label>
                     <input type="text" name="movie-showdate" placeholder="" />

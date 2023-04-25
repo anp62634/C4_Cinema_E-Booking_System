d74ff51a1d67c9f3e29bf426019c7c0db389f653
cav90839 <75345004+cav90839@users.noreply.github.com>
2023-04-25 16:34:21 -0400

    updated

--- ORM/src/main/java/org/example/model/TicketType.java
+++ ORM/src/main/java/org/example/model/TicketType.java
@@ -1,55 +1,41 @@
-/*package org.example.model;
-
+package org.example.model;
 import jakarta.persistence.*;
-
 @Entity
 @Table(name = "TicketType")
 public class TicketType {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "typeID")
     private int typeId;
-
     @Column(name = "typeName")
     private String typeName;
-
     @Column(name = "price")
     private double price;
-
-    // Constructors, getters and setters
-
+   
     public TicketType() {}
-
+    
     public TicketType(String typeName, double price) {
         this.typeName = typeName;
         this.price = price;
     }
-
     public int getTypeId() {
         return typeId;
     }
-
     public void setTypeId(int typeId) {
         this.typeId = typeId;
     }
-
     public String getTypeName() {
         return typeName;
     }
-
     public void setTypeName(String typeName) {
         this.typeName = typeName;
     }
-
     public double getPrice() {
         return price;
     }
-
     public void setPrice(double price) {
         this.price = price;
     }
-
     @Override
     public String toString() {
         return "TicketType{" +
@@ -59,5 +45,6 @@ public class TicketType {
                 '}';
     }
 }
+
 
 */

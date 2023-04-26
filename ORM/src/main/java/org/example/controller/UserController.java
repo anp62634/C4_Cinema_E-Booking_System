8ec22c97525e7e5d6afb4005355cccceacf7be45
cav90839 <75345004+cav90839@users.noreply.github.com>
2023-04-26 15:22:38 -0400

    Updated email stuff

--- ORM/src/main/java/org/example/controller/UserController.java
+++ ORM/src/main/java/org/example/controller/UserController.java
@@ -21,8 +21,17 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/save")
-    public ResponseEntity<?> saveUser(@RequestBody UserDTO userDTO){
+    public ResponseEntity<?> saveUser(@RequestBody UserDTO userDTO) throws MessagingException {
         UserResponse registrationResponse = userService.addUser(userDTO);
+               String title = "<html><body>"
+                + "<h2>C4 Cinema-e-booking Verification</h2>\n"
+                + "<p>" + "Hi, "+ userDTO.getFirstName() + "<p>"+ "We're happy you signed up for C4 Cinema.</p>\n"
+                + "<p>Please save the code below to verify and activate your account:</p>" + userDTO.getCode()
+                + "<br>"
+                + "<p>Thank you.</p>"
+                + "</body></html>";
+        String text = "C4 Cinema";
+        emailService.sendMail(userDTO.getEmail(), title, text);
         return ResponseEntity.ok(registrationResponse);
     }
 
